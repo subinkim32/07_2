85474aa794410983e7eae9bb308321c9a8a6b603
subinkim32 <salgeum1222@gmail.com>
2023-10-19 21:40:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter);
 int main(int argc, char *argv[]) {
-	int i;
-	//use static int or change temp to global variable
-	
-	for (i = 0; i < 5; i++)
-	{
-		static int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++;
-	}
+	int i = 10;
+	printf("i before function call = %d\n", i);
+	i = inc(i);
+	// inc(i) only prints 10, 10
+	printf("i after function call = %d\n", i);
 
 	return 0;
 }
 
+int inc(int counter)
+{
+	counter++;
+	return counter;
+}
