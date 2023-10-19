6bce753c3099baa3e7cbeec806f36d9d6a7eb73d
subinkim32 <salgeum1222@gmail.com>
2023-10-19 21:36:53 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	int i;
+	//use static int or change temp to global variable
 	
-	int flag = 1;
-	int y;
-	
-	while (flag != 0)
+	for (i = 0; i < 5; i++)
 	{
-		int y;
-		y = 3;
-		flag = 0;
+		static int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
 	}
-	y = 4;
-	
+
 	return 0;
 }
+
